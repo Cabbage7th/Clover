f4a1f59f3bf6a20c961cab6d97695a54a381ad08
Cabbage <hellocabbage@qq.com>
2025-06-26 13:27:43 +0800

    feat(plugin): add layzdev for plugin development

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -544,6 +544,18 @@ local default_plugins = {
 			vim.cmd("vmap <Leader>a: <cmd>:Tabularize /:\zs<CR>")
 		end,
 	},
+	---- neovim plugins for development
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
 -- lazy main entry
 require("lazy").setup(default_plugins, require("plugins.lazy_nvim"))
