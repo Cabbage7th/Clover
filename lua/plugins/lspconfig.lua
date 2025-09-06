67859f4540b40b3b4a2a353d218fb80011b2810c
Cabbage <hellocabbage@qq.com>
2025-09-06 09:06:03 +0800

    feat(plugin): update configuration of lspconfig to support neovim 0.11+

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,91 +1,40 @@
 local M = {}
 -- export on_attach & capabilities for custom lspconfigs
 
-M.on_attach = function(client, bufnr)
-	require("mappings").lspconfig(bufnr)
-
-	-- dim code not used
-	if not vim.g.lsp_semantic_tokens then
-		client.server_capabilities.semanticTokensProvider = nil
-	end
-	local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = "󰋼 " }
-	for type, icon in pairs(signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-	end
-	-- attach nvim-navic
-	local navic = require("nvim-navic")
-	if client.server_capabilities.documentSymbolProvider then
-		navic.attach(client, bufnr)
-	end
-end
-
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
-M.capabilities.textDocument.completion.completionItem = {
-	documentationFormat = { "markdown", "plaintext" },
-	snippetSupport = true,
-	preselectSupport = true,
-	insertReplaceSupport = true,
-	labelDetailsSupport = true,
-	deprecatedSupport = true,
-	commitCharactersSupport = true,
-	tagSupport = { valueSet = { 1 } },
-	resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
-		},
-	},
-}
-
-require("lspconfig").lua_ls.setup({
-	on_attach = M.on_attach,
-	capabilities = M.capabilities,
-
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-					[vim.fn.stdpath("data") .. "/lazy/extensions/nvchad_types"] = true,
-					[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-				},
-				maxPreload = 100000,
-				preloadFileSize = 10000,
-			},
-		},
-	},
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local bufnr = args.buf
+		if client then
+			-- remap keys
+			require("mappings").lspconfig(bufnr)
+			-- dim code not used
+			if not vim.g.lsp_semantic_tokens then
+				client.server_capabilities.semanticTokensProvider = false
+			end
+			-- attach nvim-navic
+			local navic = require("nvim-navic")
+			if client.server_capabilities.documentSymbolProvider then
+				navic.attach(client, bufnr)
+			end
+			-- custome diagnostic signs
+			local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = "󰋼 " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
+		end
+	end,
 })
-require("lspconfig").clangd.setup({
-	on_attach = M.on_attach,
-	capabilities = M.capabilities,
-	cmd = {
-		"clangd",
-		"--offset-encoding=utf-16",
-	},
-})
-require("lspconfig").bashls.setup({
-	on_attach = M.on_attach,
-	capabilities = M.capabilities,
-	single_file_support = true,
-	filetypes = { "sh", "zsh" },
-})
+
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "jedi_language_server", "cmake", "marksman", "jsonls", "bashls" }
+local servers = { "lua_ls", "bashls", "html", "cssls", "ts_ls", "jedi_language_server", "cmake", "marksman", "jsonls", "bashls", "clangd" }
 
-local lspconfig = require("lspconfig")
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		single_file_support = true,
-		on_attach = M.on_attach,
-		capabilities = M.capabilities,
-	})
-end
+vim.lsp.enable(
+	servers,
+	{
+		--capabilities = { },
+	}
+)
 
 return M
